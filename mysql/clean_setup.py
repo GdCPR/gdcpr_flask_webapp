2f75d6c0616cd877d71e16bc3c79eeebeb8857d0
yifattih <yifattih@proton.me>
2024-07-21 08:17:03 +0000

    fix(database-schema): mostly style

--- mysql/clean_setup.py
+++ mysql/clean_setup.py
@@ -9,27 +9,30 @@ for the first time!
 #####################################################################
 #####################################################################
 """
-import database_connector as db
 import logging
 from datetime import datetime
+from database_connector import dbconnection as db
 
-cursor = db.dbconnection.cursor(buffered=True)
+cursor = db.cursor(buffered=True)
 
-artsTB = "Articles"
-locTB = "Location"
-artslocrelTB = "ArticlesLocationRelation"
+ARTS_TB = "Articles"
+LOC_TB = "Location"
+ARTS_LOC_TB = "ArticlesLocationRelation"
 
-logging.warning(f"    [{datetime.now()}]    Removing tables: {artsTB}, {locTB}, {artslocrelTB}")
+logging.warning("    [%s]    Removing tables: %s, %s, %s",
+                datetime.now() ,ARTS_TB, LOC_TB, ARTS_LOC_TB)
 
-query = f"""DROP TABLE IF EXISTS {artsTB}, {locTB}, {artslocrelTB}"""
+# logging.warning(f"    [{datetime.now()}]    Removing tables: {ARTS_TB}, {LOC_TB}, {ARTS_LOC_TB}")
+
+query = f"""DROP TABLE IF EXISTS {ARTS_TB}, {LOC_TB}, {ARTS_LOC_TB}"""
 cursor.execute(query)
 
-logging.warning(f"    [{datetime.now()}]    Tables removed!")
+logging.warning("    [%s]    Tables removed!", datetime.now())
 
-logging.warning(f"    [{datetime.now()}]    Creating table: {artsTB}")
+logging.warning("    [%s]    Creating table: %s", datetime.now(), ARTS_TB)
 # Query: Create Articles table
 create_artsTb_query = f"""
-CREATE TABLE IF NOT EXISTS {artsTB} (
+CREATE TABLE IF NOT EXISTS {ARTS_TB} (
                                     ArticleID INTEGER,
                                     URL VARCHAR(1024) NOT NULL, 
                                     Headline VARCHAR(1024) NOT NULL,
@@ -40,35 +43,37 @@ CREATE TABLE IF NOT EXISTS {artsTB} (
                                     )
 """
 cursor.execute(create_artsTb_query)
-logging.warning(f"    [{datetime.now()}]    **Table created**")
+logging.warning("    [%s]    **Table created**", datetime.now())
 
-logging.warning(f"    [{datetime.now()}]    Creating table: {locTB}")
+logging.warning("    [%s]    Creating table: %s", datetime.now(), LOC_TB)
 # Query: Create Location table
 create_locTb_query = f"""
-CREATE TABLE IF NOT EXISTS {locTB} (
+CREATE TABLE IF NOT EXISTS {LOC_TB} (
                                     LocationID INTEGER,
                                     Name VARCHAR(1024),
                                     PRIMARY KEY (LocationID)
                                     )
 """
 cursor.execute(create_locTb_query)
-logging.warning(f"    [{datetime.now()}]    **Table created**")
+logging.warning("    [%s]    **Table created**", datetime.now())
 
-logging.warning(f"    [{datetime.now()}]    Creating table: {artslocrelTB}")
+logging.warning("    [%s]    Creating table: %s", datetime.now(), ARTS_LOC_TB)
 # Query: Create Article-Location Bridge table
 create_artslocrelTB_query = f"""
-CREATE TABLE IF NOT EXISTS {artslocrelTB} (
+CREATE TABLE IF NOT EXISTS {ARTS_LOC_TB} (
                                             ArticleID INTEGER NOT NULL,
                                             LocationID INTEGER NOT NULL,
-                                            FOREIGN KEY (ArticleID) REFERENCES {artsTB}(ArticleID),
-                                            FOREIGN KEY (LocationID) REFERENCES {locTB}(LocationID),
+                                            FOREIGN KEY (ArticleID)
+                                            REFERENCES {ARTS_TB}(ArticleID),
+                                            FOREIGN KEY (LocationID)
+                                            REFERENCES {LOC_TB}(LocationID),
                                             INDEX (ArticleID, LocationID),
                                             UNIQUE (ArticleID, LocationID)
                                             )
 """
 cursor.execute(create_artslocrelTB_query)
-logging.warning(f"    [{datetime.now()}]    **Table created**")
+logging.warning("    [%s]    **Table created**", datetime.now())
 
-logging.warning(f"    [{datetime.now()}]    Schema cleaned and ready!")
+logging.warning("    [%s]    Schema cleaned and ready!", datetime.now())
 
-db.dbconnection.commit()
+db.commit()
