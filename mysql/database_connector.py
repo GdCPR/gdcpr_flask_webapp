d8af960ff36bfb79061115c51b0adbfa870051cc
yifattih <yifattih@proton.me>
2024-07-21 08:01:14 +0000

    fix(database-schema): style mostly

--- mysql/database_connector.py
+++ mysql/database_connector.py
@@ -1,14 +1,13 @@
 import mysql.connector
 
-dbparameters= {"database": "db",
-                "host": "mysql",
+dbparameters = {"host": "mysql",
                 "user": "root",
                 "password": "root",
                 "database": "db",
                 "port": 3306}
 
-dbconnection= mysql.connector.connect(host=dbparameters["host"],
-                                      user=dbparameters["user"],
-                                      password=dbparameters["password"],
-                                      database=dbparameters["database"],
-                                      port=dbparameters["port"])
+dbconnection = mysql.connector.connect(host=dbparameters["host"],
+                                       user=dbparameters["user"],
+                                       password=dbparameters["password"],
+                                       database=dbparameters["database"],
+                                       port=dbparameters["port"])
