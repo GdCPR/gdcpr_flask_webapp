e9a5d81b2f69f473e55b3d7371d6cdcc5c0306a7
yifattih <yifattih@proton.me>
2024-08-06 20:02:27 +0000

    feat(webapp): created helpers module

---
+++ .devcontainer/flask-web-app/src/helpers/constants_db.py
@@ -0,0 +1,15 @@
+container_db_credentials = {
+    "host": "database",
+    "user": "root",
+    "password": "root",
+    "database": "db",
+    "port": 3306
+}
+
+clever_cloud_db_credentials = {
+    "host": "bghaq4iuyetqoqst4joj-mysql.services.clever-cloud.com",
+    "database": "bghaq4iuyetqoqst4joj",
+    "user": "u9fokfnntfnmr4lp",
+    "password": "avebeLzZSSrM73Ck6DkS",
+    "port": 3306
+}
