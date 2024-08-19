e7c5b9acc31bd9dc7853bb7989ddb0152a0e0d30
yifattih <yifattih@proton.me>
2024-08-19 19:27:11 +0000

    feat(webapp): svg map integrated; navigation btns implemented; some css style

--- .devcontainer/flask-web-app/src/testingStuff/static/styles/scripts.js
+++ .devcontainer/flask-web-app/src/testingStuff/static/styles/scripts.js
@@ -1,7 +1,61 @@
 function openNav() {
-  document.getElementById("myMenu").style.width = "25%";
+  document.getElementById("myMenu").style.width = "100%";
+  console.log("Menu opened")
 }
 
 function closeNav() {
   document.getElementById("myMenu").style.width = "0";
+  console.log("Menu closed")
 }
+
+// start map zoom-pan
+var transformMatrix = [1, 0, 0, 1, 0, 0];
+var svg = document.getElementById('map');
+var viewbox = svg.getAttributeNS(null, "viewBox").split(" ");
+var centerX = parseFloat(viewbox[2]) / 2;
+var centerY = parseFloat(viewbox[3]) / 2;
+var matrixGroup = svg.getElementById("matrix-group");
+
+function pan(dx, dy) {
+    transformMatrix[4] += dx;
+    transformMatrix[5] += dy;
+    var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
+    matrixGroup.setAttributeNS(null, "transform", newMatrix);
+    logMessage = 
+    `
+    Panning function:
+    svg with transform:${newMatrix}
+    `;
+    console.log(logMessage);
+}
+
+function zoom(scale) {
+    for (var i = 0; i < transformMatrix.length; i++) {
+        transformMatrix[i] *= scale;
+    }
+
+    transformMatrix[4] += (1 - scale) * centerX;
+    transformMatrix[5] += (1 - scale) * centerY;
+
+    var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
+    matrixGroup.setAttributeNS(null, "transform", newMatrix);
+    logMessage = 
+    `
+    Zoom function:
+    svg with transform:${newMatrix}
+    `;
+    console.log(logMessage)
+}
+
+function home() {
+  transformMatrix = [1, 0, 0, 1, 0, 0];
+  var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
+  matrixGroup.setAttributeNS(null, "transform", newMatrix);
+  logMessage = 
+  `
+  Home function:
+  svg with transform:${newMatrix}
+  `;
+  console.log(logMessage)
+}
+// end map zoom-pan
